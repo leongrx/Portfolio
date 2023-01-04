404bfaa7c4d4aad96cc54ebb88cf24878a6ef810
Leon Groschek <leon.groschek12@gmail.com>
2023-01-04 11:03:29 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IntroductionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Component, Injectable, Input } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Injectable()
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  public aboutMe: boolean = false;
+  public skills: boolean = false;
+  public portfolio: boolean = false;
+  
+  constructor(private appComponent: AppComponent) { }
+  
+  // closeImprint() {
+  //   debugger
+  //   this.appComponent['closeImprint']();
+  // }
+
+  select(key: string) {
+    switch (key) {
+      case 'aboutMe':
+        this.aboutMe = true;
+        this.skills = false;
+        this.portfolio = false;
+        break;
+      case 'skills':
+        this.aboutMe = false;
+        this.skills = true;
+        this.portfolio = false;
+        break;
+      default:
+        this.aboutMe = false;
+        this.skills = false;
+        this.portfolio = true;
+        break;
+    }
+  }
+
+  
+  public resetSelect() {
+    this.aboutMe = false;
+    this.skills = false;
+    this.portfolio = false;
+  }
+
+}
